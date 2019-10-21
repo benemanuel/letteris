6f032e037b5e6d47d3b19eb4bcd93b8b74a6de2f
Avi BenEmanuel <benemanuel@geulah.org.il>
2019-10-21 12:33:09 +0300

    editing

---
+++ Rebuild.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd Tools
+backtogether.bat
+cd ..
--- Tools/backtogether.bat
+++ Tools/backtogether.bat
@@ -1,3 +1,3 @@
-#to take all parts together again
+rem to take all parts together again
 copy ..\header.txt ..\letteris_utf8.txt
 for /F "tokens=1,2" %%a in (..\booksnames.txt) do type ..\%%b_utf8.txt >> ..\letteris_utf8.txt
