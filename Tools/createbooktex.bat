e461ed66cbb83d232791a459fb246857076d660b
Avi BenEmanuel <benemanuel@geulah.org.il>
2020-05-02 19:08:12 +0300

    Edited until  Joshua 15:1

--- Tools/createbooktex.bat
+++ Tools/createbooktex.bat
@@ -1,17 +1,15 @@
 @echo off
 SETLOCAL
-FOR /F "tokens=* USEBACKQ" %%F IN (`dir C:\Users\avi\GitHub\letteris\%1_utf8.txt /aa /b`) DO (
-SET var=%%F
-goto :exist
-)
-goto :end
-
-:exist
-attrib C:\Users\avi\GitHub\letteris\%1_utf8.txt -a
-echo html %1
-copy head.h temp\%1.html
-echo  %1 %2 >> temp\%1.html
-echo ^</br^> ^</h2^> >> temp\%1.html
-for /f "tokens=1,2,3,4,*" %%a in ('TYPE C:\Users\avi\GitHub\letteris\%1_utf8.txt') do echo (%%b,%%c) %%e >>  temp\%1.html
-echo ^</html^> >> temp\%1.html
-:end
+FOR /F "tokens=* USEBACKQ" %%F IN (`attrib C:\Users\avi\GitHub\letteris\%1_utf8.txt`) DO SET var=%%F
+IF "%VAR:~0,1%" == "A" GOTO NOFILE
+:YESFILE
+EXIT /B 0
+:NOFILE
+rem   @ECHO %1 Changed.
+   attrib C:\Users\avi\GitHub\letteris\%1_utf8.txt -a
+   echo html %1
+   copy head.h temp\%1.html
+   echo  %1 %2 >> temp\%1.html
+   echo ^</br^> ^</h2^> >> temp\%1.html
+   for /f "tokens=1,2,3,4,*" %%a in ('TYPE C:\Users\avi\GitHub\letteris\%1_utf8.txt') do echo (%%b,%%c) %%e >>  temp\%1.html
+   echo ^</html^> >> temp\%1.html
