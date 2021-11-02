c419e3d79aa607fdf8dff7d7d1bcc959baa9242d
Avi BenEmanuel <benemanuel@geulah.org.il>
2021-11-02 07:47:11 +0200

    Edited until  Proverbs 15:28

--- Tools/Rebuild.cmd
+++ Tools/Rebuild.cmd
@@ -1,15 +1,15 @@
 @echo off
 notepad %userprofile%\GitHub\letteris\readme.md
 echo start
-for /F "tokens=2,4" %%a in ('findstr "until" %userprofile%\GitHub\letteris\README.md') do set CLIPBOARD_TEXT=" Edited until  %%a %%b"
+for /F "tokens=2,4" %%a in ('findstr "until" %userprofile%\GitHub\letteris\README.md') do set CLIPBOARD_TEXT="Edited until  %%a %%b"
 echo %CLIPBOARD_TEXT% | clip
-for /F "tokens=2,4" %%a in ('findstr "until" %userprofile%\GitHub\letteris\README.md') do echo #Edited %date% until  %%a %%b > %date%.tmp
+for /F "tokens=2,4" %%a in ('findstr "until" %userprofile%\GitHub\letteris\README.md') do echo #Edited %date% until  %%a %%b > tmp.tmp
 cd %userprofile%\GitHub\letteris\Tools
 echo backtogether - to take all parts together again
-copy ..\header.txt + ..\%date%.tmp ..\letteris.utf8_txt 
+copy ..\header.txt + ..\tmp.tmp ..\letteris.utf8_txt 
 for /F "tokens=1,2" %%a in (..\booksnames.txt) do type ..\%%b.utf8_txt >> ..\letteris.utf8_txt
 echo updatehtml - create html files
-copy head.h + ..\%date%.tmp + body.h temp\index.html /y
+copy head.h + ..\tmp.tmp + body.h temp\index.html /y
 echo if no notice then run rebuild2.cmd %CLIPBOARD_TEXT%
 for /F "tokens=1,2,3" %%a in (%userprofile%\GitHub\letteris\booksnames.txt) DO createbooktex.bat %%b %%c
 echo run rebuild2.cmd %CLIPBOARD_TEXT%
--- Tools/backtogether.bat
+++ Tools/backtogether.bat
@@ -1,7 +1,7 @@
 @echo off
 echo backtogether - to take all parts together again
-copy ..\header.txt + ..\%date%.tmp ..\letteris.utf8_txt 
+copy ..\header.txt + ..\tmp.tmp ..\letteris.utf8_txt 
 for /F "tokens=1,2" %%a in (..\booksnames.txt) do type ..\%%b.utf8_txt >> ..\letteris.utf8_txt
 updatehtml.bat
-del %date%.tmp /q
-del ..\%date%.tmp /q
+rem del %date%.tmp /q
+rem del ..\%date%.tmp /q
